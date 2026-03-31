54e4a38260965f8b3f8f9893eeac24df57b54c0b
Anthony Alanya <anthonyalanya088@gmail.com>
2026-03-31 08:09:30 -0500

    yml-test

--- src/test/java/edu/pe/cibertec/infracciones/InfraccionesApplicationTests.java
+++ src/test/java/edu/pe/cibertec/infracciones/InfraccionesApplicationTests.java
@@ -1,16 +1,13 @@
 package edu.pe.cibertec.infracciones;
-
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class InfraccionesApplicationTests {
 
-	@Disabled("Deshabilitado temporalmente en CI porque falla al cargar ApplicationContext")
 	@Test
 	void contextLoads() {
-		// Ignorado temporalmente para que el pipeline pase
+
 	}
 
 }
